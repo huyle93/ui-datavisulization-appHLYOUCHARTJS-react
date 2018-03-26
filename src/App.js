dfe690cdb824442d9c82ca2a4c8e7376f4de2120
huyle93 <huyle.finance@gmail.com>
2018-03-25 23:24:22 -0400

    set up components. created bar chart. set up dependencies. app working, bar chart show

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Chart from "./components/Chart"
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Async call for API
+    this.setState({
+      chartData: {
+        labels: ['Bob', 'Alice', 'Mike', 'Ken', 'Yost', 'Collette'],
+        datasets: [
+            {
+                label: 'Probability of Winning',
+                data: [
+                    617594,
+                    181045,
+                    153060,
+                    106519,
+                    105162,
+                    95072
+                ],
+                backgroundColor:[
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)',
+                    'rgba(255, 99, 132, 0.6)'
+                ]
+            }
+        ]
+    }
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">React Chart JS</h1>
+        <Chart chartData={this.state.chartData} judge="Thomas Durkin" legendPosition="bottom"/>
       </div>
     );
   }
